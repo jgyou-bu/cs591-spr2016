fe2735663bda37753cffb70e2abd8ff86f1f77eb
jgyou-bu <jgyou@bu.edu>
2016-06-12 16:59:53 -0400

    because pet peeve == typos

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # cs591-spr2016
 
-Test area for courework for spring 2016 cs591 data mechanics
+Test area for coursework for spring 2016 cs591 data mechanics.
+
+See [https://github.com/jgyou-bu/course-2016-spr-proj-two] (https://github.com/jgyou-bu/course-2016-spr-proj-two) for fork from data mechanics, as well as [https://github.com/Data-Mechanics/course-2016-spr-proj] (https://github.com/Data-Mechanics/course-2016-spr-proj) for latest version of classmates' projects.
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import urllib.request
+import urllib.parse
 import json
 
 '''
@@ -12,10 +13,17 @@ print(json.dumps(json.loads(response), sort_keys=True, indent=2))
 url2 = "https://data.cityofboston.gov/resource/vjvb-2kg6.json?area=boston"
 response2 = urllib.request.urlopen(url2).read().decode("utf-8")
 boston_pantries = json.loads(response2)
+# prints out one row of table in unfriendly format
 print(boston_pantries[0])
 
 # prints out json in friendly format
 print(json.dumps(json.loads(response2), sort_keys=True, indent=4))
+
+#employee earnings
+url4 = "https://data.cityofboston.gov/resource/wypd-uw2t.json?title=" + urllib.parse.quote_plus("Admin Secretary")
+response4 = urllib.request.urlopen(url4).read().decode("utf-8")
+employees = json.loads(response4)
+print(employees)
 
 
 # building energy and water use metrics
